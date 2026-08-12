ad7bdfa7be236c07e966bd3acf0a2fcd99b49047
Yoser <yoserrr@gmail.com>
2026-08-12 10:12:59 -0600

    feat: practice variables

--- 01-variables.js
+++ 01-variables.js
@@ -1,5 +1,18 @@
 let nombre = "Yoser";
 let edad = 35;
+let altura = 1.72;
+let estudiante  = true;
+let ciudad = "Zarcero";
+let peso = 74;
+let tieneTrabajo = false;
+let peliculaFavorita = "Jason Bourne Saga";
+
 
 console.log(nombre);
 console.log(edad);
+console.log(altura);
+console.log(estudiante);
+console.log(ciudad);
+console.log(peso);
+console.log(tieneTrabajo);
+console.log(peliculaFavorita);
