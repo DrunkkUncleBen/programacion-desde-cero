a0b4d6cdfc5e05205833df210a947ef45e701728
Yoser <yoserrr@gmail.com>
2026-08-18 09:15:42 -0600

    feat: practice operators

---
+++ 02-operadores.js
@@ -0,0 +1,81 @@
+console.log("");
+
+console.log("=== EJERCICIO NUMBER ===");
+
+let number1 = 10;
+let number2 = 3;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+
+console.log("");
+
+console.log("=== EJERCICIO YEARSOLD ===");
+
+let yearsold = 34;
+
+console.log(yearsold > 18);
+console.log(yearsold < 18);
+console.log(yearsold >= 18);
+console.log(yearsold <= 18);
+console.log(yearsold === 18);
+console.log(yearsold !== 18);
+
+console.log("");
+
+console.log("=== EJERCICIO CARS ===");
+//Test
+let cars = 35
+
+console.log(cars > 65);
+console.log(cars < 65);
+console.log(cars >= 35);
+console.log(cars <= 35);
+console.log(cars === 35);
+console.log(cars !== 35);
+
+console.log("");
+
+console.log("=== EJERCICIO AGE ===");
+
+let age = 25;
+
+console.log(age >= 18 && age <= 30);
+console.log(age < 18 || age > 60);
+console.log(age === 25 && cars === 35);
+
+console.log("");
+
+console.log("=== EJERCICIO NOT ===");
+
+let isStudent = true
+
+console.log(!isStudent);
+console.log(!(age === 25));
+console.log(!(cars < 20));
+
+console.log("");
+
+console.log("=== EJERCICIO DRIVING ===");
+
+let drivingAge = 22;
+let hasLicense = true;
+
+console.log(drivingAge >= 18 && hasLicense);
+
+console.log("");
+
+console.log("=== EJERCICIO PARTY ===");
+
+let partyAge = 16;
+let withAdult = true;
+
+console.log(partyAge >= 18 || withAdult);
+
+console.log("");
+
+console.log("=== EJERCICIO DRIVING2 ===");
+
+console.log(!(drivingAge >= 18 && hasLicense));
