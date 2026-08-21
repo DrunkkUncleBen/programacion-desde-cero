cde6353d9a7f969ed28af96bb4aece711a28f276
Yoser <yoserrr@gmail.com>
2026-08-21 10:21:51 -0600

    feat: practice for loops

---
+++ 04-for.js
@@ -0,0 +1,43 @@
+console.log("=== EJERCICIO 1 ===");
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+console.log("=== EJERCICIO 2 ===");
+
+for (let i = 2; i <= 20; i = i + 2) {
+    console.log(i);
+}
+
+console.log("=== EJERCICIO 3 ===");
+
+for (let i = 1; i <= 10; i++) {
+        if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+console.log("=== EJERCICIO 4 ===");
+
+for (let i = 1; i <= 10; i++) {
+        if (i % 2 === 1) {
+        console.log(i);
+    }
+}
+
+console.log("=== EJERCICIO 5 ===");
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0) {
+        console.log(i);
+    }
+}
+
+console.log("=== RETO FINAL ===");
+
+for (let i = 1; i <= 30; i++) {
+    if (i % 3 === 0) {
+        console.log(i, "Múltiplo de 3");
+    }
+}
