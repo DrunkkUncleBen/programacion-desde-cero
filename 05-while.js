55365acede128e2b7e5104887c29dbff1d3bc680
Yoser <yoserrr@gmail.com>
2026-08-24 09:09:44 -0600

    feat: practice while loops

---
+++ 05-while.js
@@ -0,0 +1,53 @@
+console.log("=== EJERCICIO 1 ===");
+
+let i = 1;
+
+while (i <= 10) {
+    console.log(i);
+    i++;
+}
+//----
+
+console.log("=== EJERCICIO 2 ===");
+
+let countdown  = 5;
+
+while (countdown >= 1) {
+    console.log(countdown);
+    countdown--;
+}
+//----
+
+console.log("=== EJERCICIO 3 ===");
+
+let number = 1;
+
+while (number <= 20) {
+    if(number % 2 === 0)
+    console.log(number);
+    number++;
+}
+//----
+
+console.log("=== EJERCICIO 4 ===");
+
+let cuentaregresiva = 10;
+
+while (cuentaregresiva >= 1) {
+    console.log(cuentaregresiva);
+    cuentaregresiva--;
+}
+
+console.log("¡Despegue!");
+//----
+
+console.log("=== RETO FINAL ===");
+
+let intentos = 1;
+
+while (intentos <= 3) {
+    console.log("Intento", intentos);
+    intentos++;
+}
+
+console.log("Se acabaron los intentos");
